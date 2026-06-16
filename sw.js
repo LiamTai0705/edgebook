913a30bee4de2b9ec7df5ec90b1495a749f39774
LiamTai0705 <yam60619@gmail.com>
2026-06-16 22:46:50 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='edgebook-v18';
+const CACHE='edgebook-v19';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
